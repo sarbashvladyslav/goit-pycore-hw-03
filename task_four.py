addbb189aed9e22f010c8c6a946db0133476a1de
sarbashvladyslav <sarbash.vladyslav@gmail.com>
2025-10-12 17:39:10 +0200

    update HW 4

--- task_four.py
+++ task_four.py
@@ -2,25 +2,24 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 users = [
-    {"name": "John Doe", "birthday": "1985.10.12"},
-    {"name": "Charlie Davis", "birthday": "1995.10.15"},
-    {"name": "David Brown", "birthday": "1993.1.16"},
-    {"name": "Jane Smith", "birthday": "1990.10.18"},
-    {"name": "Eve Wilson", "birthday": "1988.10.20"},
-    {"name": "Alice Johnson", "birthday": "1975.9.20"},
+    {"name": "John Doe", "birthday": "1985.11.1"},
+    {"name": "Charlie Davis", "birthday": "1995.11.2"},
+    {"name": "David Brown", "birthday": "1993.11.1"},
+    {"name": "Jane Smith", "birthday": "1990.10.25"},
+    {"name": "Eve Wilson", "birthday": "1988.10.30"},
+    {"name": "Alice Johnson", "birthday": "1975.9.28"},
     {"name": "Bob Brown", "birthday": "2000.11.15"},
     {"name": "Grace Lee", "birthday": "1992.12.14"},
-    {"name": "Hank Miller", "birthday": "1983.11.16"},
+    {"name": "Hank Miller", "birthday": "1983.11.29"},
 ]
 
 def get_upcoming_birthdays(users):
-    today = datetime.today().date()
+    #today = datetime.today().date()
+    today = datetime(2025, 10, 30).date()
     new_list = []
-
     for user in users:
         parsed_birthday_date = datetime.strptime(user['birthday'], "%Y.%m.%d").date()
         birthday_this_year = datetime(month=parsed_birthday_date.month, day=parsed_birthday_date.day, year=today.year).date()
-        current_month = birthday_this_year.month == today.month
         birthday_within_a_week = 0 <= (birthday_this_year - today).days <= 7
         birthday_on_saturday = birthday_this_year.isoweekday() == 6
         birthday_on_sunday = birthday_this_year.isoweekday() == 7
@@ -32,7 +31,7 @@ def get_upcoming_birthdays(users):
             # relativedelta - для коректного переходу на наступний рік (високосний рік і т.д.)
             # congratulation_date = datetime.strftime(birthday_this_year + relativedelta(years=+1), "%Y-%m-%d")
             # new_list.append({"name": user["name"], "congratulation_date": congratulation_date})
-        elif birthday_within_a_week and current_month:
+        elif birthday_within_a_week:
             if birthday_on_saturday:
                 congratulation_date = datetime.strftime(birthday_this_year + timedelta(days=2), "%Y-%m-%d") 
                 new_list.append({"name": user["name"], "congratulation_date": congratulation_date})
